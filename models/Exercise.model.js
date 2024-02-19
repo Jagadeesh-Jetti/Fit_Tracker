e9df8cbcd6857ff051247cf60b308953aadf48c7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-19 13:22:42 +0530

    feat: added a exercise model

---
+++ models/Exercise.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const exerciseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    caloriesBurned: {
+      type: Number,
+      default: function () {
+        const caloriesPerMinute = {
+          walking: 5,
+          running: 10,
+          swimming: 12,
+          dancing: 7,
+          yoga: 3,
+          cycling: 8,
+          dancing: 7,
+        };
+        const exerciseType = this.name.toLowerCase();
+        const caloriesPerMinuteForExercise =
+          caloriesPerMinute[exerciseType] || 5;
+        return (this.duration = caloriesPerMinuteForExercise);
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Exercise = mongoose.Model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
