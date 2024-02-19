aa80a14947699177c24daf28b2099695ce8feef8
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-19 15:11:24 +0530

    refactor: improved the post goal logic

--- routers/Goal.router.js
+++ routers/Goal.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const ExpressRouter = express.Router();
+const GoalRouter = express.Router();
 const Goal = require("../models/Goal.model");
 
 GoalRouter.get("/", async (req, res) => {
   try {
     const allGoals = await Goal.find();
-    res.status(200).json({ message: "ALl goals fetched:", allGoals });
+    res.status(200).json({ message: "All goals fetched:", allGoals });
   } catch (error) {
     res.status(500).json({ message: "Error while fetching all goals:", error });
   }
@@ -16,7 +16,13 @@ GoalRouter.post("/", async (req, res) => {
   const { name, description, calories, status } = req.body;
   try {
     if (name && description && calories && status) {
-      const goalAdded = await Goal({ name, description, calories, status });
+      const goalAdded = new Goal({
+        name,
+        description,
+        calories,
+        status,
+      });
+      await goalAdded.save();
       res.status(201).json({ message: "New goal added", goalAdded });
     } else {
       res.status(400).json({ message: "Missing required fields" });
@@ -31,11 +37,11 @@ GoalRouter.delete("/:id", async (req, res) => {
   try {
     const goalDeleted = await Goal.findByIdAndDelete(goalId);
     if (!goalDeleted) {
-      res.status(404).json({ error: "Couldnt find the goal to delete" });
+      res.status(404).json({ error: "Could not find the goal to delete" });
     }
-    res.status(201).json({ message: "Deleted the goal successfully" });
+    res.status(200).json({ message: "Deleted the goal successfully" });
   } catch (error) {
-    res.status(500).json({ error: "Error while deleting the goal" });
+    res.status(500).json({ error: "Error while deleting the goal", error });
   }
 });
 
