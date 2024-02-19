f71e4b3a3cf4949eb40fb1226e29637f9da52811
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-19 15:06:45 +0530

    refactor: added types for more security

--- models/Food.model.js
+++ models/Food.model.js
@@ -2,11 +2,26 @@ const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema(
   {
-    name: String,
-    calories: Number,
-    protein: Number,
-    carbs: Number,
-    fat: Number,
+    name: {
+      type: String,
+      required: true,
+    },
+    calories: {
+      type: Number,
+      required: true,
+    },
+    protein: {
+      type: Number,
+      required: true,
+    },
+    carbs: {
+      type: Number,
+      required: true,
+    },
+    fat: {
+      type: Number,
+      required: true,
+    },
   },
   {
     timestamps: true,
