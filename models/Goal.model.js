aa4cc3b71e4edfe9570da0f4ffef5875086dc218
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-19 15:07:04 +0530

    refactor: added type to name

--- models/Goal.model.js
+++ models/Goal.model.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const goalSchema = new mongoose.Schema(
   {
-    name: String,
+    name: {
+      type: String,
+      required: true,
+    },
     description: String,
     targetDate: Date,
     calories: Number,
