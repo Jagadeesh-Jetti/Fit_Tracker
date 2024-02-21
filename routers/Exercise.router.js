ca0759f2e0b1ef4ca0b749f0bfb06db378ea2a1a
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-21 17:41:48 +0530

    refactor: formatted the response structue and status codes

--- routers/Exercise.router.js
+++ routers/Exercise.router.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const ExerciseRouter = express.Router();
 const Exercise = require("../models/Exercise.model");
 
 ExerciseRouter.get("/", async (req, res) => {
   try {
     const allExercises = await Exercise.find();
-    res.status(200).json(allExercises);
+    res.status(200).json({ data: allExercises });
   } catch (error) {
     res
       .status(500)
@@ -15,15 +14,20 @@ ExerciseRouter.get("/", async (req, res) => {
 });
 
 ExerciseRouter.post("/", async (req, res) => {
-  const { name, duration } = req.body;
   try {
-    if (name && duration) {
-      const exerciseAdded = new Exercise(req.body);
-      await exerciseAdded.save();
-      res.status(201).json({ message: "New exercise added", exerciseAdded });
-    } else {
-      res.status(400).json({ error: "Missing required fields" });
+    const { name, duration } = req.body;
+
+    if (!name || !duration || duration <= 0) {
+      return res
+        .status(400)
+        .json({ error: "Invalid or missing fields in the request" });
     }
+
+    const exerciseAdded = new Exercise(req.body);
+    await exerciseAdded.save();
+    res
+      .status(201)
+      .json({ message: "New exercise added", data: exerciseAdded });
   } catch (error) {
     res.status(500).json({ error: "Error while adding a new exercise", error });
   }
@@ -37,7 +41,12 @@ ExerciseRouter.delete("/:id", async (req, res) => {
     if (!exerciseDeleted) {
       res.status(404).json({ error: "Couldn't find the exercise in the db" });
     } else {
-      res.status(200).json({ message: "Deleting the exercise is successful" });
+      res
+        .status(200)
+        .json({
+          message: "Deleting the exercise is successful",
+          data: exerciseDeleted,
+        });
     }
   } catch (error) {
     res.status(500).json({ error: "Error while deleting the exercise", error });
